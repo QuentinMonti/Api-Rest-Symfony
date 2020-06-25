98eb92f08678cb53850955cafa6edf00aeb67432
Quentin <quentin.monteferrario@gmail.com>
2020-06-25 12:09:57 +0200

    request post with datetime

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -1,12 +1,17 @@
 <?php
 
 namespace App\Controller;
+
+use App\Entity\Post;
 use Symfony\Component\Serializer\SerializerInterface;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\JsonResponse ;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class ApiPostController extends AbstractController
 {
@@ -22,5 +27,23 @@ class ApiPostController extends AbstractController
         $response = new JsonResponse($json, 200, [], true); 
         
         return $response;
+    }
+
+
+    /**
+     * @Route("/api/post", name="api_post_store", methods={"POST"})
+     */
+    public function store(Request $request, SerializerInterface $serializer, EntityManagerInterface $em)
+    {
+        $jsonRecu = $request->getContent();
+
+        $post = $serializer->deserialize($jsonRecu, Post::class, 'json');
+        
+        $post->setCreatedAt(new \DateTime());
+
+        $em->persist($post);
+        $em->flush();
+
+        dd($post);
     }
 }
